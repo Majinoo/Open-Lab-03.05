e95e4784c1f53bbefddaf4594eeb78a1809d1ddf
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-09-29 23:05:57 +0200

    Implement the method that compares two strings regardless of their case.

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            throw new NotImplementedException();
+            str1 = str1.ToUpper();
+            str2 = str2.ToUpper();
+            int x = str1.CompareTo(str2);
+            if (x != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
